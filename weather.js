974701b953a11e8e985f66fdba203df730a0c086
BishantRajbanshi <rajbancbishant@gmail.com>
2023-12-31 01:41:14 +0545

    wn

---
+++ weather.js
@@ -0,0 +1,64 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.box-search button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+search.addEventListener('click',() => {
+
+    const APIKey ='c55f438e699a31156c728056b77cea2a'; 
+    const city= document.querySelector('.search-bar input').value;
+
+    if(city =='')
+      return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json =>{
+
+      const image = document.querySelector('.weather-box img');
+      const temperature = document.querySelector('.weather-box .temperature');
+      const description = document.querySelector('.weather-box .description');
+      const humidity = document.querySelector('.weather-details .humidity span');
+      const wind = document.querySelector('.weather-details .wind span');
+      const pressure = document.querySelector('.weather-details .pressure span');
+
+      switch (json.weather[0].main) {
+        case 'Clear':
+          image.src = 'clear.png';
+          break;
+
+        case 'Rain':
+          image.src = 'rain.png';
+          break;
+
+        case 'Snow':
+          image.src = 'snow.png';
+          break;
+        
+        case 'Clouds':
+          image.src = 'cloud.png';
+          break;
+        
+        case 'Mist':
+          image.src = 'mist.png';
+          break;
+
+        case 'Haze':
+          image.src = 'mist.png';
+          break;
+
+        default:
+          image.src = 'cloud.png';
+          break;
+      }
+
+      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+      description.innerHTML = `${json.weather[0].description}`;
+      humidity.innerHTML = `${json.main.humidity}%`;
+      wind.innerHTML = `${jason.main.wind.speed}Km/h`;
+      pressure.innerHTML = `${jason.main.pressure}atm`;
+
+
+    });
+
+
+
+    
+});
