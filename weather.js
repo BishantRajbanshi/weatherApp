dd8675c4815e953dfdcd1c56d1a36e7deb201487
ramankarki <ramankarki40@gmail.com>
2023-12-31 16:51:10 +0545

    fix: typos

--- weather.js
+++ weather.js
@@ -1,64 +1,60 @@
-const container = document.querySelector('.container');
-const search = document.querySelector('.box-search button');
-const weatherBox = document.querySelector('.weather-box');
-const weatherDetails = document.querySelector('.weather-details');
-search.addEventListener('click',() => {
+const container = document.querySelector('.container')
+const search = document.querySelector('.search')
+const weatherBox = document.querySelector('.weather-box')
+const weatherDetails = document.querySelector('.weather-details')
 
-    const APIKey ='c55f438e699a31156c728056b77cea2a'; 
-    const city= document.querySelector('.search-bar input').value;
+search.addEventListener('click', () => {
+	const APIKey = 'c55f438e699a31156c728056b77cea2a'
+	const city = document.querySelector('.search-bar').value
 
-    if(city =='')
-      return;
+	if (city == '') return
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json =>{
+	fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+	)
+		.then((response) => response.json())
+		.then((json) => {
+			const image = document.querySelector('.weather-box img')
+			const temperature = document.querySelector('.weather-box .temperature')
+			const description = document.querySelector('.weather-box .description')
+			const humidity = document.querySelector('.weather-details .humidity span')
+			const wind = document.querySelector('.weather-details .wind span')
+			const pressure = document.querySelector('.pressure span')
 
-      const image = document.querySelector('.weather-box img');
-      const temperature = document.querySelector('.weather-box .temperature');
-      const description = document.querySelector('.weather-box .description');
-      const humidity = document.querySelector('.weather-details .humidity span');
-      const wind = document.querySelector('.weather-details .wind span');
-      const pressure = document.querySelector('info.pressure span');
+			switch (json.weather[0].main) {
+				case 'Clear':
+					image.src = 'clear.png'
+					break
 
-      switch (json.weather[0].main) {
-        case 'Clear':
-          image.src = 'clear.png';
-          break;
+				case 'Rain':
+					image.src = 'rain.png'
+					break
 
-        case 'Rain':
-          image.src = 'rain.png';
-          break;
+				case 'Snow':
+					image.src = 'snow.png'
+					break
 
-        case 'Snow':
-          image.src = 'snow.png';
-          break;
-        
-        case 'Clouds':
-          image.src = 'cloud.png';
-          break;
-        
-        case 'Mist':
-          image.src = 'mist.png';
-          break;
+				case 'Clouds':
+					image.src = 'cloud.png'
+					break
 
-        case 'Haze':
-          image.src = 'mist.png';
-          break;
+				case 'Mist':
+					image.src = 'mist.png'
+					break
 
-        default:
-          image.src = 'cloud.png';
-          break;
-      }
+				case 'Haze':
+					image.src = 'mist.png'
+					break
 
-      temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-      description.innerHTML = `${json.weather[0].description}`;
-      humidity.innerHTML = `${json.main.humidity}%`;
-      wind.innerHTML = `${json.main.wind.speed}Km/h`;
-      pressure.innerHTML = `${json.main.pressure}atm`;
+				default:
+					image.src = 'cloud.png'
+					break
+			}
 
-
-    });
-
-
-
-    
-});
+			temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`
+			description.innerHTML = `${json.weather[0].description}`
+			humidity.innerHTML = `${json.main.humidity}%`
+			wind.innerHTML = `${json.wind.speed}Km/h`
+			pressure.innerHTML = `${json.main.pressure}atm`
+		})
+})
